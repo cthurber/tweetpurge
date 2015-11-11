bfbce38009563e18a0b442d396730e11d00fe1ba
Chris Thurber <cthurber@me.com>
2015-11-11 00:01:10 -0500

    Implementing search in C (mostly just for fun...)

---
+++ search.c
@@ -0,0 +1,6 @@
+// Args as search terms
+// search for each arg in line in csv
+// write tweet_id, date, and tweet_text to hidden
+	// Possible to get output directly back into python?
+	// From commandline..? <- promising
+// return number of tweets found
